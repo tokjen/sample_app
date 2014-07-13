457a42c119207bdfa638d3baf9225e8aa18a88d6
tokjen <tokjen@gmail.com>
2014-07-13 15:47:13 +0800

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -82,35 +82,48 @@ describe "Authentication" do
           it { should have_title('Sign in') }
         end
       end
-     end
 
-     describe "as wrong user" do
-       let(:user) { FactoryGirl.create(:user) }
-       let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
-       before { sign_in user,no_capybara: true }
+      describe "in the Microposts controller" do
 
-       describe "submitting a GET request to the Users#edit action" do
-         before { get edit_user_path(wrong_user) }
-         specify { expect(response.body).not_to match(full_title('Edit user')) }
-         specify { expect(response).to redirect_to(root_url) }
-       end
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
 
-       describe "submitting a PATCH request to the Users#update action" do
-         before { patch user_path(wrong_user) }
-         specify { expect(response).to redirect_to(root_url) }
-       end
-     end
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+    end
 
-     describe "as non-admin user" do
-       let(:user) { FactoryGirl.create(:user) }
-       let(:non_admin) { FactoryGirl.create(:user) }
+    describe "as wrong user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+      before { sign_in user,no_capybara: true }
 
-       before { sign_in non_admin, no_capybara: true }
+      describe "submitting a GET request to the Users#edit action" do
+        before { get edit_user_path(wrong_user) }
+        specify { expect(response.body).not_to match(full_title('Edit user')) }
+        specify { expect(response).to redirect_to(root_url) }
+      end
 
-       describe "submitting a DELETE request to the Users#destroy action" do
-         before { delete user_path(user) }
-         specify { expect(response).to redirect_to(root_url) }
-       end
-     end
-   end
+      describe "submitting a PATCH request to the Users#update action" do
+        before { patch user_path(wrong_user) }
+        specify { expect(response).to redirect_to(root_url) }
+      end
+
+      describe "as non-admin user" do
+        let(:user) { FactoryGirl.create(:user) }
+        let(:non_admin) { FactoryGirl.create(:user) }
+
+        before { sign_in non_admin, no_capybara: true }
+
+        describe "submitting a DELETE request to the Users#destroy action" do
+          before { delete user_path(user) }
+          specify { expect(response).to redirect_to(root_url) }
+        end
+      end
+    end
+  end
 end
